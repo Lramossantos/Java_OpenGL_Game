ab1e0e2996d466b85658db04ceb84e4f52c4e4cf
Leonardo Ramos <leonardoramosdossantos92@gmail.com>
2024-12-02 23:30:00 -0300

    Update EventListener.java
    Coloquei uma forma 2D  quadrada no meio da tela

--- src/org/graphics/EventListener.java
+++ src/org/graphics/EventListener.java
@@ -15,10 +15,10 @@ public class EventListener implements GLEventListener{
 		
 		gl.glColor3f(0, 0, 1);
 		gl.glBegin(GL2.GL_QUADS);
-		gl.glVertex2f(-0.5f, -0.5f);
-		gl.glVertex2f(0.5f, -0.5f);
-		gl.glVertex2f(0.5f, 0.5f);
-		gl.glVertex2f(-0.5f, 0.5f);
+		gl.glVertex2f(-50, -50);
+		gl.glVertex2f(50, -50);
+		gl.glVertex2f(50, 50);
+		gl.glVertex2f(-50, 50);
 		gl.glEnd();
 		
 		
@@ -33,14 +33,20 @@ public class EventListener implements GLEventListener{
 	@Override
 	public void init(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
-		gl.glClearColor(1, 0, 0, 1);
+		gl.glClearColor(0, 0, 0, 1);
 		
 		
 	}
 
 	@Override
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-		//
+		GL2 gl = drawable.getGL().getGL2();
+		
+		gl.glMatrixMode(GL2.GL_PROJECTION);
+		gl.glLoadIdentity();
+		
+		gl.glOrtho(-320, 320, -180, 180, -1, 1);
+		gl.glMatrixMode(GL2.GL_MODELVIEW);
 		
 	}
 	
