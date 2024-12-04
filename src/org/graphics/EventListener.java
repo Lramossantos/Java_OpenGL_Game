3bafbd08dcdb5f1241b95df1fae5a7b35bd6ed3a
Leonardo Ramos <leonardoramosdossantos92@gmail.com>
2024-12-03 22:34:52 -0300

    Renderer.java
    Fiz um quadrado que redimenciona de acordo com o tamanho da tela e apliquei a tela cheia no meu game com o
    [ window.setFullscreen(true); ]

--- src/org/graphics/EventListener.java
+++ src/org/graphics/EventListener.java
@@ -5,49 +5,45 @@ import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 
-public class EventListener implements GLEventListener{
+public class EventListener implements GLEventListener {
 
 	@Override
 	public void display(GLAutoDrawable drawable) {
-		GL2 gl = drawable.getGL().getGL2();	
-		
+		GL2 gl = drawable.getGL().getGL2();
+
 		gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
-		
+
 		gl.glColor3f(0, 0, 1);
 		gl.glBegin(GL2.GL_QUADS);
-		gl.glVertex2f(-50, -50);
-		gl.glVertex2f(50, -50);
-		gl.glVertex2f(50, 50);
-		gl.glVertex2f(-50, 50);
+		gl.glVertex2f(-1, -1);
+		gl.glVertex2f(1, -1);
+		gl.glVertex2f(1, 1);
+		gl.glVertex2f(-1, 1);
 		gl.glEnd();
-		
-		
 	}
 
 	@Override
 	public void dispose(GLAutoDrawable drawable) {
-		//
-		
+
 	}
 
 	@Override
 	public void init(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
-		gl.glClearColor(0, 0, 0, 1);
-		
-		
+		gl.glClearColor(1, 0, 0, 1);
 	}
 
 	@Override
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
 		GL2 gl = drawable.getGL().getGL2();
-		
+
 		gl.glMatrixMode(GL2.GL_PROJECTION);
 		gl.glLoadIdentity();
-		
-		gl.glOrtho(-320, 320, -180, 180, -1, 1);
+
+		float unitsTall = Renderer.getWindowHeight() / (Renderer.getWindowWidth() / Renderer.unitsWide);
+
+		gl.glOrtho(-Renderer.unitsWide / 2, Renderer.unitsWide / 2, -unitsTall / 2, unitsTall / 2, -1, 1);
 		gl.glMatrixMode(GL2.GL_MODELVIEW);
-		
 	}
-	
+
 }
--- src/org/graphics/Renderer.java
+++ src/org/graphics/Renderer.java
@@ -5,28 +5,40 @@ import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.util.FPSAnimator;
 
-
 public class Renderer {
 	private static GLWindow window = null;
-	
+
+	public static int screenWidth = 640;
+	public static int screenHeight = 360;
+
+	public static float unitsWide = 10;
+
 	public static void init() {
 		GLProfile.initSingleton();
 		GLProfile profile = GLProfile.get(GLProfile.GL2);
 		GLCapabilities caps = new GLCapabilities(profile);
-		
+
 		window = GLWindow.create(caps);
-		window.setSize(640, 360);
+		window.setSize(screenWidth, screenHeight);
 		window.setResizable(false);
 		window.addGLEventListener(new EventListener());
-		
+
 		FPSAnimator animator = new FPSAnimator(window, 60);
 		animator.start();
 		
-		window.setVisible(true);		
+		window.setFullscreen(true);		
+		window.setVisible(true);
 	}
-	
-	public static void main(String[] args ) {
-		init();	
-		
+
+	public static int getWindowWidth() {
+		return window.getWidth();
+	}
+
+	public static int getWindowHeight() {
+		return window.getHeight();
+	}
+
+	public static void main(String[] args) {
+		init();
 	}
 }
